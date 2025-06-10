2cc82c6775c126bd0ad132feda28b3758af72398
mandati <nareshreddymandati@gmail.com>
2025-06-10 20:38:41 +0530

    change in vite file

--- server/vite.ts
+++ server/vite.ts
@@ -23,7 +23,7 @@ export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
     middlewareMode: true,
     hmr: { server },
-    allowedHosts: true,
+    allowedHosts: ['localhost'], // Changed from boolean to string array
   };
 
   const vite = await createViteServer({
